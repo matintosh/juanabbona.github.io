2bcde2ffd1cf849ce14964c01e5048df1ba8e381
Juan Abbona <juan.abbona97@gmail.com>
2020-06-23 19:56:47 -0300

    New version of website

--- js/main.0a2ab2c75825ffe8f06a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{e6Wu:function(n,e,o){"use strict";o.r(e);o("SYky"),o("EFQ4"),o("nH3I");var u=o("EVdn"),i=o.n(u);o("hZTp");i()(document).ready(()=>{setTimeout((function(){i()(".loader").hide()}),250)})},hZTp:function(n,e,o){}},[["e6Wu",1,2]]]);
